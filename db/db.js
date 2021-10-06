bb7b61cc9bdbc9cafbdc5d6c53318a6ad03b9e9e
Lawrence Sun <alawrencesun@gmail.com>
2021-10-06 00:11:27 -0700

    added dotenv

--- db/db.js
+++ db/db.js
@@ -4,9 +4,9 @@ require('dotenv').config()
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: 'Questions',
+  database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
-  port: 5432
+  port: process.env.DB_PORT
 });
 
 module.exports = pool;
